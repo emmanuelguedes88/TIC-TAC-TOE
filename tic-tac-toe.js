57f82b13467dd7ccfa886f91f5b4cf2a617c32ed
Emmanuel Guedes <emmanuel_guedes@hotmail.com>
2022-11-21 15:23:51 -0300

    Inserindo os síombolos

--- tic-tac-toe.js
+++ tic-tac-toe.js
@@ -1,16 +1,32 @@
 const tic_tac_toe = {
     board: ['','','','','','','','',''],
-    simbols: ['x','0'],
+    simbols: {
+        options: ['x','0'],
+        turn_index: 0,
+        change: function(){
+            this.turn_index= (this.turn_index === 0 ? 1 : 0);
+          }
+ },
     container_element: null,
+    gameover: false,
+
     init: function(container){
         this.container_element = container;
     },
 
+make_play: function(position){
+    if (this.gameover) return false;
+    if (this.board[position] === ''){
+        this.board[position] = this.simbols.options [this.simbols.turn_index];
+        this.draw();
+        this.simbols.change();
+    }
+},
     draw: function(){
         let content= '';
 
         for (i in this.board) {
-            content += '<div>' + i + '</div>';
+            content += '<div onclick= "tic_tac_toe.make_play(' + i + ')">' + this.board[i] + '</div>';
         }
         this.container_element.innerHTML=content;
     }
